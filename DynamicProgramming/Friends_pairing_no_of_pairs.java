f0743809a1bf1d60bcca650859cf513a17a9235c
Ashish Goswami <agoswami841@gmail.com>
2021-04-16 17:41:31 +0530

    Algorithms

---
+++ DynamicProgramming/Friends_pairing_no_of_pairs.java
@@ -0,0 +1,18 @@
+public class Friends_pairing_no_of_pairs {
+    public static void main(String args[]){
+        int n = 4;
+        countPair(n);
+    }
+
+    public static void countPair(int n ){
+        int[] dp  = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2]*(i-1);
+        }
+        System.out.println(dp[n]);
+    }  
+
+}
